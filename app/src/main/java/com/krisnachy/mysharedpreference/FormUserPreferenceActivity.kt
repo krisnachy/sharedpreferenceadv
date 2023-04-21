8759ca8c47077fb38a667ae6e6056333fbaeae06
krisnachy <cahyayuana@gmail.com>
2023-04-21 09:38:53 +0700

    add new activity

---
+++ app/src/main/java/com/krisnachy/mysharedpreference/FormUserPreferenceActivity.kt
@@ -0,0 +1,11 @@
+package com.krisnachy.mysharedpreference
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FormUserPreferenceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_user_preference)
+    }
+}
